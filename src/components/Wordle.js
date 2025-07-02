23f5028ada9757734562037d0bfc7205cbed55aa
John Felton <feltonjsd@gmail.com>
2025-07-02 23:23:48 +0100

    add tick and question mark for letters in contrast mode

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -559,6 +559,22 @@ const Wordle = ({ onBackToMenu }) => {
                 >
                   {/* Show the correct answer letters in white for the revealed answer row, regardless of animation */}
                   {rowIndex === revealedAnswerRow ? targetWord[index] : (rowIndex === currentRow ? currentGuess[index] || '' : guesses[rowIndex][index] || '')}
+                  {isContrastMode && evaluations[rowIndex] && revealedLetters[rowIndex][index] && (
+                    <div className="contrast-icon">
+                      {evaluations[rowIndex][index] === 'correct' && (
+                        <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="currentColor">
+                          <path d="M0 0h24v24H0V0z" fill="none"/>
+                          <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z"/>
+                        </svg>
+                      )}
+                      {evaluations[rowIndex][index] === 'wrong-position' && (
+                        <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="currentColor">
+                          <path d="M0 0h24v24H0V0z" fill="none"/>
+                          <path d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"/>
+                        </svg>
+                      )}
+                    </div>
+                  )}
                   
                 </div>
               ))}
@@ -595,6 +611,22 @@ const Wordle = ({ onBackToMenu }) => {
                   data-key={key}
                 >
                   {key}
+                  {isContrastMode && letterStates[key] && (
+                    <div className="contrast-icon">
+                      {letterStates[key] === 'correct' && (
+                        <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="currentColor">
+                          <path d="M0 0h24v24H0V0z" fill="none"/>
+                          <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z"/>
+                        </svg>
+                      )}
+                      {letterStates[key] === 'wrong-position' && (
+                        <svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="currentColor">
+                          <path d="M0 0h24v24H0V0z" fill="none"/>
+                          <path d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"/>
+                        </svg>
+                      )}
+                    </div>
+                  )}
                 </button>
               ))}
               {i === 2 && (
