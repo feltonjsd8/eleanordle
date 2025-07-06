dc140e1d77c02d49cb9915954abbd258335517e7
John Felton <john.felton@gmail.com>
2025-07-06 12:48:15 +0100

    change definition icon

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -579,9 +579,9 @@ const Wordle = ({ onBackToMenu }) => {
                     title={`View definition of ${state.guesses[rowIndex]}`}
                     aria-label={`View definition of ${state.guesses[rowIndex]}`}
                   >
-                    <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 0 24 24" width="30px" fill="currentColor">
-                      <path d="M0 0h24v24H0V0z" fill="none"/>
-                      <path d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"/>
+                    {/* Open Book icon from Wikimedia Commons, CC0 */}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none">
+                      <path d="M2 6.5C2 5.12 3.12 4 4.5 4H19.5C20.88 4 22 5.12 22 6.5V19.5C22 20.33 21.33 21 20.5 21C17.46 21 14.42 20.5 12 19.5C9.58 20.5 6.54 21 3.5 21C2.67 21 2 20.33 2 19.5V6.5ZM4.5 6C4.22 6 4 6.22 4 6.5V18.5C6.97 18.5 9.97 18.03 12 17.13C14.03 18.03 17.03 18.5 20 18.5V6.5C20 6.22 19.78 6 19.5 6H4.5Z" fill="#111"/>
                     </svg>
                   </button>
                 )}
