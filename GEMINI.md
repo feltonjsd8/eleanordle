7fef1f1e9e634f51f6bc08c6a3937d1b05df82c4
John Felton <john.felton@gmail.com>
2025-07-10 17:57:06 +0100

    change streak to icons

---
+++ GEMINI.md
@@ -0,0 +1,1 @@
+Do not generate unit tests.
--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -633,9 +633,19 @@ const Wordle = ({ onBackToMenu }) => {
       />
       <div className="game-header">
         <div className="header-content">
-          <div style={{ position: 'absolute', left: 12, top: 12, textAlign: 'left', fontWeight: 600, fontSize: 16, color: '#333' }}>
-            <div>Streak: {state.streak}</div>
-            <div>Score: {state.totalScore}</div>
+          <div className="score-streak-container">
+            <div className="streak-display">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-fire" viewBox="0 0 16 16">
+                <path d="M8 16c3.314 0 6-2 6-5.5 0-1.5-.5-4-2.5-6 .25 1.5-1.25 2-1.25 2C11 4 9 .5 6 0c.357 2 .5 4-2 6-1.25 1-2 2.729-2 4.5C2 14 4.686 16 8 16m0-1c-1.657 0-3-1-3-2.75 0-.75.25-2 1.25-3C6.125 10 7 10.5 7 10.5c-.375-1.25.5-3.25 2-3.5-.179 1-.25 2 1 3 .625.5 1 1.364 1 2.25C11 14 9.657 15 8 15"/>
+              </svg>
+              <span>{state.streak}</span>
+            </div>
+            <div className="score-display">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/>
+              </svg>
+              <span>{state.totalScore}</span>
+            </div>
           </div>
           <h1>
             <span className="title-highlight">Eleanor</span>dle
