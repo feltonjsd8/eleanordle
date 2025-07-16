00ac52581adf47f728b32a8afac3484116d0bce0
John Felton <john.felton@gmail.com>
2025-07-16 09:07:53 +0100

    scale font size for mobile

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,27 +1,34 @@
 import React from 'react';
 
 const Logo = () => (
-  <svg width="220" height="50" viewBox="0 0 220 50" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Eleanordle">
+  <svg className="eleanordle-logo" viewBox="0 0 220 50" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Eleanordle">
     <title>Eleanordle</title>
     <style>
       {`
+        .eleanordle-logo {
+          width: 100%;
+          height: auto;
+          max-width: 220px;
+          display: block;
+          margin: 0 auto;
+        }
         @import url('https://fonts.googleapis.com/css2?family=Fredoka+One&family=Roboto:wght@700&display=swap');
         .eleanordle-logo-eleanor {
           font-family: 'Fredoka One', cursive;
-          font-size: 28px;
+          font-size: 24px;
           fill: #e91e63;
         }
         .eleanordle-logo-dle {
           font-family: 'Fredoka One', cursive;
-          font-size: 28px;
+          font-size: 24px;
           /*font-weight: 700;*/
           fill: #9c627f98;
         }
       `}
     </style>
-    <text x="10" y="35" aria-hidden="true">
-      <tspan className="eleanordle-logo-eleanor">Elean</tspan>
-      <tspan className="eleanordle-logo-dle" dx="1">ordle</tspan>
+    <text x="50%" y="35" textAnchor="middle" aria-hidden="true">
+      <tspan className="eleanordle-logo-eleanor">Eleanor</tspan>
+      <tspan className="eleanordle-logo-dle" dx="-2">dle</tspan>
     </text>
   </svg>
 );
